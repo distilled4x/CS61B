0d756786d5ddd34c49a61faa30fe10cd76eadd73
distilled4x <matt.huck92@gmail.com>
2019-03-20 19:06:23 -0700

    for submission

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -4,4 +4,113 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 
+    private final int N;
+    private int size;
+    private WeightedQuickUnionUF board;
+    private WeightedQuickUnionUF boardCount;
+
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new java.lang.IllegalArgumentException();
+        }
+
+        this.N = N;
+        this.size = N * N;
+        this.board = new WeightedQuickUnionUF(size + 2);
+        this.boardCount = new WeightedQuickUnionUF(size + 1);
+
+    }
+
+    public void open(int row, int col) {
+        if (isBounded(row, col)) {
+            int coord = xyTo1D(row, col);
+            boardCount.union(coord, size);
+
+            if (coord <= N - 1) {
+                board.union(coord, N * N);
+
+            } else if (!percolates() && coord > N * (N - 1)) {
+                board.union(coord, N * N + 1);
+            }
+
+            tileConnect(row, col);
+
+        }
+    }
+
+    public boolean isOpen(int row, int col) {
+        if (isBounded(row, col)) {
+            int coord = xyTo1D(row, col);
+            return boardCount.connected(coord, size);
+
+        }
+
+        return false;
+
+    }
+
+    public boolean isFull(int row, int col) {
+        if (isBounded(row, col)) {
+            return board.connected(xyTo1D(row, col), N * N);
+        }
+
+        return false;
+    }
+
+
+    public int numberOfOpenSites() {
+
+        return size - boardCount.count() + 1;
+
+    }
+
+    public boolean percolates() {
+        return board.connected(N * N, N * N + 1);
+    }
+
+    public void tileConnect(int row, int col) {
+        int coord = xyTo1D(row, col);
+
+        if (isOpen(row + 1, col)) {
+            board.union(coord, xyTo1D(row + 1, col));
+
+        }
+
+        if (isOpen(row - 1, col)) {
+            board.union(coord, xyTo1D(row - 1, col));
+
+        }
+
+        if (isOpen(row, col + 1)) {
+            board.union(coord, xyTo1D(row, col + 1));
+
+        }
+
+        if (isOpen(row, col - 1)) {
+            board.union(coord, xyTo1D(row, col - 1));
+
+        }
+    }
+
+    public boolean isBounded(int row, int col) {
+        int coord = xyTo1D(row, col);
+        if (coord < 0 || coord > N * N - 1) {
+            return false;
+            //throw new java.lang.IndexOutOfBoundsException();
+        }
+
+        return true;
+    }
+
+    public int xyTo1D(int row, int col) {
+        return row * N + col;
+    }
+
+    public static void main(String args[]) throws java.lang.IndexOutOfBoundsException {
+        Percolation perc = new Percolation(10);
+        System.out.println(perc.isBounded(0,6));
+        perc.open(0,6);
+        System.out.println(perc.isOpen(0,6));
+    }
+
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,58 @@
 package hw2;
 
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
+
 public class PercolationStats {
+    private int N;
+    private int T;
+    private PercolationFactory pf;
+    private double[] permutations;
+
+
+    // run T trials of N size percolation sims, store amount of openings until percolation
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        this.N = N;
+        this.T = T;
+        this.pf = pf;
+        permutations = new double[T];
+
+        for (int i = 0; i < T; i++) {
+            Percolation perc = pf.make(N);
+
+            while(!perc.percolates()) {
+                int row = StdRandom.uniform(N);
+                int col = StdRandom.uniform(N);
+
+                if (!perc.isOpen(row, col)) {
+                    perc.open(row, col);
+                }
+
+            }
+
+            permutations[i] = (double) perc.numberOfOpenSites();
+        }
+
+    }
+
+    public double mean() {
+        return StdStats.mean(permutations);
+    }
+
+    public double stddev() {
+        return StdStats.stddev(permutations);
+    }
+
+    public double confidenceLow() {
+        return mean() - confidence();
+    }
+
+    public double confidenceHigh() {
+        return mean() + confidence();
+    }
+
+    public double confidence() {
+        return (1.96 * stddev()) / Math.sqrt((double) T);
+    }
 
 }
